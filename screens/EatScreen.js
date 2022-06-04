2c86554e86de0804e53c6076a72abbece86d4dad
Tonyaap <s.kniest@thesio.eu>
2022-06-04 19:20:57 +0200

    commit this now!

---
+++ screens/EatScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EatScreen = () => {
+    return (
+        <View>
+            <Text>Here is the map!</Text>
+        </View>
+    )
+}
+
+export default EatScreen
+
+const styles = StyleSheet.create({
+    background: 'gray'
+})
